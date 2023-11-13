8899937dad77bec1e111529b6fb68d1c9163a5f4
JesPedro <jesuspcaes@gmail.com>
2023-11-12 22:56:35 -0500

    Lab_13

---
+++ Laboratorio13/RRHHEF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRHHEF
+{
+    public class Class1
+    {
+    }
+}
